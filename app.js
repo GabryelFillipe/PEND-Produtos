38991f48e7961664222b92cb479f674953c331fa
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-08-20 09:39:22 -0300

    Adicionando função de geração de cards, falta arrumar erro no import

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 'use strict'
+
+import produtos from "./produtos_atualizados.json" with{type: "json"}
+
+function criarProdutos(produto){
+
+    const containerProdutos = document.getElementById('produto')
+    const card = document.createElement('div')
+
+    const imagemProduto = document.createElement('img')
+    imagemProduto.src = produto.imagem
+
+    const nomeProduto = document.createElement('span')
+    nomeProduto.textContent = produto.nome
+
+    const descricaoProduto = document.createElement('p')
+    descricaoProduto.textContent = produto.descricao
+    
+    const preco = document.createElement('p')
+    preco.textContent = produto.preco
+
+    const categoria = document.createElement('p')
+    categoria.textContent = produto.categoria
+
+    containerProdutos.appendChild(card)
+    card.appendChild(imagemProduto)
+    card.appendChild(nomeProduto)
+    card.appendChild(descricaoProduto)
+    card.appendChild(preco)
+    card.appendChild(categoria)
+}
+function carregarProdutos(){
+    produto.forEach(criarProdutos)
+}
+carregarProdutos()
