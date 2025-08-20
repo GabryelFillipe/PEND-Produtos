34d806384768124b8eafa14f0aed6cf40285c066
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-08-20 10:26:22 -0300

    Erro do import corrigido, falta corrigir exibição dos produtos

--- app.js
+++ app.js
@@ -2,25 +2,32 @@
 
 import produtos from "./produtos_atualizados.json" with{type: "json"}
 
-function criarProdutos(produtos_atualizados){
+function criarProdutos(produtos){
 
     const containerProdutos = document.getElementById('produto')
     const card = document.createElement('div')
+    card.classList.add('cards')
 
     const imagemProduto = document.createElement('img')
-    imagemProduto.src = produto.imagem
+    imagemProduto.src = produtos.imagem
+    
 
     const nomeProduto = document.createElement('span')
-    nomeProduto.textContent = produto.nome
+    nomeProduto.textContent = produtos.nome
+    nomeProduto.classList.add('nome')
+
 
     const descricaoProduto = document.createElement('p')
-    descricaoProduto.textContent = produto.descricao
+    descricaoProduto.textContent = produtos.descricao
+    descricaoProduto.classList.add('descricao')
     
     const preco = document.createElement('p')
-    preco.textContent = produto.preco
+    preco.textContent = produtos.preco
+    preco.classList.add('preco')
 
     const categoria = document.createElement('p')
-    categoria.textContent = produto.categoria
+    categoria.textContent = produtos.categoria
+    categoria.classList.add('categoria')
 
     containerProdutos.appendChild(card)
     card.appendChild(imagemProduto)
@@ -30,6 +37,6 @@ function criarProdutos(produtos_atualizados){
     card.appendChild(categoria)
 }
 function carregarProdutos(){
-    produto.forEach(criarProdutos)
+    produtos.forEach(criarProdutos)
 }
 carregarProdutos()
