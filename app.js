2497b9bef4b5ae5dab72b8622a0bfb64253f3dd4
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-09-02 08:35:54 -0300

    Iniciando função para melhor exibição do produto ao clicar nele

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ function criarProdutos(produtos, containerDestino,) {
     const imagemProduto = document.createElement('img')
     imagemProduto.src = produtos.imagem
 
+    const idProduto = produtos.id
+    
 
     const nomeProduto = document.createElement('span')
     nomeProduto.textContent = produtos.nome
@@ -47,7 +49,7 @@ function criarProdutos(produtos, containerDestino,) {
     const classificacaoProduto = document.createElement('h3')
     classificacaoProduto.textContent = carregarClassificacao(produtos.classificacao)
 
-    mensagem(card, nomeProduto)
+    mensagem(card, nomeProduto, idProduto)
 
     containerDestino.appendChild(card)
     card.appendChild(imagemProduto)
@@ -85,10 +87,53 @@ carregarProdutos()
 
 
 
-function mensagem(card, nomeProduto) {
+function mensagem(card, nomeProduto, idProduto) {
     card.addEventListener('click', () => {
         alert(`O produto selecionado foi ${nomeProduto.textContent}`)
+       
+        
+
     })
+}
+function criarSpan(produtos){
+
+        const spanProdutos = document.createElement('div')
+        spanProdutos.classList.add('span-produtos')
+        spanProdutos.setAttribute('id', 'span-produtos')
+
+        const imagemProduto = document.createElement('img')
+        imagemProduto.src = produtos.imagem
+
+        const textoSpan = document.createElement('div')
+        textoSpan.classList.add('texto-span')
+
+        const nomeProduto = document.createElement('span')
+        nomeProduto.textContent = produtos.nome
+        nomeProduto.classList.add('nome')
+
+
+        const descricaoProduto = document.createElement('p')
+        descricaoProduto.textContent = produtos.descricao
+        descricaoProduto.classList.add('descricao')
+
+        const preco = document.createElement('p')
+        preco.textContent = produtos.preco
+        preco.classList.add('preco')
+
+        const categoria = document.createElement('p')
+        categoria.textContent = produtos.categoria
+        categoria.classList.add('categoria')
+
+        const classificacaoProduto = document.createElement('h3')
+        classificacaoProduto.textContent = carregarClassificacao(produtos.classificacao)
+
+        spanProdutos.appendChild(imagemProduto)
+        spanProdutos.appendChild(textoSpan)
+        textoSpan.appendChild(nomeProduto)
+        textoSpan.appendChild(descricaoProduto)
+        textoSpan.appendChild(preco)
+        textoSpan.appendChild(classificacaoProduto)
+        textoSpan.appendChild(categoria)
 
 }
 
